3e3db65cbf1c79a98f16da440aac4766453a679f
SaarthakPopa <116756158+SaarthakPopa@users.noreply.github.com>
2026-01-09 12:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def create_final_json(uploaded_files):
 
         metric_block = {
             "id": idx,
-            "metric": f"DER_{uploaded_file}",
+            "metric": f"DER_{uploaded_file[name]}",
             "level": "l2",
             "supported_customers": {
                 "included": [],
@@ -330,5 +330,6 @@ elif app_choice == "DER ZIP Data Compiler":
         except Exception as e:
             st.error("❌ Error processing CSV files")
             st.exception(e)
+
 
 
